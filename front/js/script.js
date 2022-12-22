4ad5343b1a3a215d53173e25626898141c9c4a1f
Aliona Uzun <aliona-balti@outlook.fr>
2022-12-22 14:30:44 +0100

    producs inserted in the home page

--- front/js/script.js
+++ front/js/script.js
@@ -2,12 +2,57 @@ fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
 .then((data) => addProducts(data))
 
-function addProducts(products){
-    const imageUrl = products[0].imageUrl
-
+function addProducts(donnes){
+        donnes.forEach((kanap) => {
+        
+        const {_id, imageUrl, altTxt, name, description} = kanap
+        const anchor = makeAnchor(_id)
+        const article = document.createElement("article")
+        const image = makeImage(imageUrl, altTxt)
+        const h3 = makeH3(name)
+        const p = makeParagraph(description)
+        
+        appendElementsToArticle(article, [image,h3, p])
+        appendArticleToAnchor(anchor, article)
+    })
+    }
+function appendElementsToArticle(article, array) {
+    array.forEach((item) => {
+        article.appendChild(item)
+    })
+}
+    
+function  makeAnchor (id){
     const anchor = document.createElement("a")
-    anchor.innerHTML = "je suis un lien"
-    anchor.href = imageUrl
-    const listNode = document.getElementById("items")
-    listNode.appendChild(anchor)
+    anchor.href = "./product.html?id=" + id
+    return anchor
+}   
+
+function appendArticleToAnchor(anchor, article){
+    const items = document.getElementById("items")
+    items.appendChild(anchor)
+    anchor.appendChild(article)
+}
+
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    image.removeAttribute("title")
+    image.removeAttribute("style")
+    return image
+}
+
+function makeH3(name) {
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+
+function makeParagraph(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
 }
